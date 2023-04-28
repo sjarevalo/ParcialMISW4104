d941861406b843f346a5c0de0e86b962b7f81665
Steven Arevalo <sjarevalo.ext@ayesa.com>
2023-04-27 19:47:48 -0500

    Created coffe module and class

---
+++ src/app/coffee/coffee.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoffeeComponent } from './coffee.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CoffeeComponent]
+})
+export class CoffeeModule { }
