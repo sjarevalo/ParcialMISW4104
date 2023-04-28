fa705bfc95a5fec9152665cfdf661816b424ca13
Steven Arevalo <sjarevalo.ext@ayesa.com>
2023-04-27 22:51:20 -0500

    Added count of type

--- src/app/coffee/coffee-list/coffee-list.component.ts
+++ src/app/coffee/coffee-list/coffee-list.component.ts
@@ -9,10 +9,23 @@ import { CoffeeService } from '../coffee.service';
 })
 export class CoffeeListComponent implements OnInit {
   coffes: Array<Coffee> = [];
+  origen: number = 0;
+  blend: number = 0;
   constructor(private coffeeService: CoffeeService) { }
   getCoffees(): void{
-    this.coffeeService.getCoffes().subscribe(resp => this.coffes = resp )
+    this.coffeeService.getCoffes().subscribe(resp => {
+      this.coffes = resp
+      this.coffes.forEach(cf =>{
+        if(cf.tipo.toLowerCase() === 'blend'){
+          this.blend++
+        }else if(cf.tipo.toLowerCase().includes('origen')){
+          this.origen++
+        }
+       })
+     })
+
   }
+
   ngOnInit() {
     this.getCoffees();
   }
